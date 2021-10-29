9cca1e2475c1d240effef325a78ec748c313ada9
Christian Lessa <christianclessa@gmail.com>
2021-10-29 18:34:11 -0300

    Requisito 8 concluido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
-    return (<div data-testid="missions" />);
+    return (
+      <>
+        <div data-testid="missions" />
+        <Title headline="Missões" />
+      </>
+    );
   }
 }
